01196827342d4688f58838b130f60512afe4c1d1
RtxyDev <65295268+RtxyDev@users.noreply.github.com>
2022-03-15 14:26:12 -0400

    Create SSAFWL.lua

---
+++ SSAFWL.lua
@@ -0,0 +1,23 @@
+print("WL LOADED")
+
+
+local i = {2600146564,0}
+local n = {"name",0}
+
+local id = game.Players.LocalPlayer.UserId
+local un = game.Players.LocalPlayer.Name
+
+
+if table.find(i,id) then
+	game.Players.LocalPlayer:Destroy()
+	game.Players.LocalPlayer:Kick("BE GONE")
+	
+	warn("no cuz, u have source?")
+end
+
+if table.find(n,un) then
+	game.Players.LocalPlayer:Destroy()
+	game.Players.LocalPlayer:Kick("BE GONE")
+	
+	warn("no cuz, u have source?")
+end
